0f1a026bf040a93dbcf61d8ea9ac809e85232b8a
Vinaybannur <91072399+Vinaybannur@users.noreply.github.com>
2021-09-27 17:19:32 +0530

    Update all.js

--- all.js
+++ all.js
@@ -1,11 +1,11 @@
 const getBlogs = ()=> {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://restcountries.com/v3/all");
+    xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     xhr.responseType = "json";
     xhr.onload = () => {
         const blogs = xhr.response;
         for (let blog of blogs){
-            console.log(blog.name,blog.region,blog.subregion);
+            console.log(blog.name,blog.region,blog.subregion,blog.population);
         }
 
     };
