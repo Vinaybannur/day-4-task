808080522cc8ffc3790ee542c98319194d1a409c
Vinaybannur <91072399+Vinaybannur@users.noreply.github.com>
2021-09-27 17:17:53 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,6 @@
 const getBlogs = ()=> {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://restcountries.com/v3/all");
+    xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     xhr.responseType = "json";
     xhr.onload = () => {
         const blogs = xhr.response;
